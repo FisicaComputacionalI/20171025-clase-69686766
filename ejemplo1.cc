b0b77f64cbe6b9dadc74cea526f451dbecb8da86
Alumno FCFM <alumno@mod302.(none)>
2017-10-25 08:46:32 -0500

    Ejercicio de clase

---
+++ ejemplo1.cc
@@ -0,0 +1,31 @@
+// Crescenciano Cuautle Coyotl
+//Ejercicio de clase 
+//25 de octubre 2017
+
+#include <iostream>
+using namespace std;
+
+int p;
+int esprimo(int x){
+  int Flag=0;
+  for (int i=2; i<=x/2; i++)
+  {
+   if(x%i==0)
+   Flag=1;
+  }
+return Flag;
+}
+int main (){
+int p;
+cout<<"Dame un valor de p"<<endl;
+cin>>p;
+do{
+   	if ((p%2)==0)
+        p=p+1;
+	if(esprimo(p)==0)
+        p=p+2;
+  }while(esprimo(p)==0);
+cout<<p<<endl;
+return 0;
+}
+
